63207fed1461c219fffcbe7cd692a367e4ee4ca3
Utkarsha-Saha <sahautkarsha29@gmail.com>
2026-08-26 23:01:55 +0530

    chore: reset P2 PR1 data branch

--- optimization/__main__.py
+++
@@ -1,4 +0,0 @@
-from .run_optimizer import main
-
-if __name__ == "__main__":
-    main()
--- optimization/supplier_allocation.py
+++
@@ -1,4 +0,0 @@
-"""Backward-compatible P4 public API."""
-from .optimizer import SupplierAllocationOptimizer, optimize_allocation
-
-__all__ = ["SupplierAllocationOptimizer", "optimize_allocation"]
